08cd61742771c5caa199972811a1ecf465ee406b
ZiadMontaser <ziadmontaser2005@gmail.com>
2026-08-02 20:36:17 +0300

    disable combiles and stress tests

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 #define all(x) (x).begin(), (x).end()
-
 using ll = long long int;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
@@ -11,32 +10,19 @@ using vl = vector<ll>;
 
 int dx[] = { 1, -1, 0 , 0 };
 int dy[] = { 0, 0, 1 , -1 };
-
 const int OO = (1 << 30), MOD = 1e9 + 7;
 const ll OOLL = (1ll << 60);
-
 const int N=1e5+7, M=5e3, MAXLOG=25;
 
-void solve() {
-    int n;
-    cin >> n;
-
-}
+void solve() { }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
 
     int t = 1;
     cin >> t;
-
-    while (t--)
-    {
-        solve();
-    }
-
-    return 0;
+    while (t--) solve();
 }
