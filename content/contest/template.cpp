ab314e59759c8a5a9739438e94a36b45ec20b9d2
ZiadMontaser <ziadmontaser2005@gmail.com>
2026-08-02 20:19:31 +0300

    first commit

---
+++ content/contest/template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+using ll = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+
+int dx[] = { 1, -1, 0 , 0 };
+int dy[] = { 0, 0, 1 , -1 };
+
+const int OO = (1 << 30), MOD = 1e9 + 7;
+const ll OOLL = (1ll << 60);
+
+const int N=1e5+7, M=5e3, MAXLOG=25;
+
+void solve() {
+    int n;
+    cin >> n;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
